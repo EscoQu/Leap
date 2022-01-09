bc99c7be9320916b9da2a7bb7b8c8469c494d183
EscoQu <escolastico.sanchez@bbva.com>
2022-01-09 18:47:42 +0000

    Commit dos

--- prueba.py
+++ prueba.py
@@ -1,1 +1,11 @@
 import networkx as nx
+from dwave.system.samplers import DWaveSampler
+import matplotlib.pyplot as mpl 
+import dwave_networkx as dnx 
+from dwave.system.samplers import DWaveSampler
+from dwave.system.composites import EmbeddingComposite
+sampler=EmbeddingComposite(DWaveSampler())
+s5=nx.star_graph(5)
+print (dnx.min_vertex_cover(s5,sampler))
+nx.draw(s5,with_labels=True)
+mpl.savefig('graficoestrella.png')
